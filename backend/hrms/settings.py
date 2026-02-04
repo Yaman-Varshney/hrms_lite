41ef982e41eaed95f49a0983bd0291578dfdc91d
Yaman Varshney <yaman@Yamans-MacBook-Air.local>
2026-02-04 14:53:47 +0530

    Adds static file location

--- backend/hrms/settings.py
+++ backend/hrms/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,6 +55,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 
