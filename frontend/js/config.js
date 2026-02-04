fd14c92ee87e75b3e2bee4140c1637d027c9a02d
Yaman Varshney <yaman@Yamans-MacBook-Air.local>
2026-02-04 18:24:13 +0530

    Changes the base url

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
-// const API_BASE = "https://yamanvarshney.pythonanywhere.com/api";
-const API_BASE = "http://localhost:8000/api";
+const API_BASE = "https://yamanvarshney.pythonanywhere.com/api";
+// const API_BASE = "http://localhost:8000/api";
 
